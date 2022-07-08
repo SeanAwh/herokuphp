413fc6c843a518e5448a9a9e9a94d13bc0e460ef
SeanAwh <20041162@myrp.edu.sg>
2022-07-08 21:35:40 +0800

    test

--- web/test.php
+++ web/test.php
@@ -5,6 +5,7 @@
 </title>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="A test page to get 100 grade on google lighthouse">
 </head>
 <body>
   Hello World!
