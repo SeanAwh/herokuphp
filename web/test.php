c71a1f91a0cb8b1c4997ef99be44c941680a34c9
SeanAwh <20041162@myrp.edu.sg>
2022-07-08 21:32:58 +0800

    test

--- web/test.php
+++ web/test.php
@@ -1,5 +1,10 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<title>
+  Test page
+</title>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
   Hello World!
